92f0df492dbed8a217494b1018860845cf71e1e5
ShahRachit <rachitshah93@gmail.com>
2022-02-22 10:42:55 +0530

    Injection literal values via Setter DI vid 38

--- spring-demo-one/src/com/learn2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/learn2code/springdemo/CricketCoach.java
@@ -4,6 +4,28 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	private String team;
+	
+	
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
 	//our setter method, this will be called by spring to inject dependency
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside setter method - setFortuneService");
