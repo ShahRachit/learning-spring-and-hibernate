5e9f5baddac0536f25796a401136ae9cd6aae143
ShahRachit <rachitshah93@gmail.com>
2022-02-23 17:36:46 +0530

    Using default bean id vid 56

--- spring-demo-annotations/src/com/learn2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/learn2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.learn2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
