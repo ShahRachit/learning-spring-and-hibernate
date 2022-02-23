1aeeaa0cc26fa1fdbe43a1bdf01aa76d5eebed70
ShahRachit <rachitshah93@gmail.com>
2022-02-23 11:23:51 +0530

    Bean scope Demo Vid 45

---
+++ spring-demo-one/src/com/learn2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,33 @@
+package com.learn2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		
+		//retrieve the bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		//check if they are the same bean
+		boolean result = (theCoach == alphaCoach);
+		
+		//print out the results
+		System.out.println("Pointing to the same object: " + result);
+		
+		System.out.println("Memory location for theCoach: " + theCoach);
+		
+		System.out.println("Memory location for alphaCoach: " + alphaCoach);
+		//call methods on bean
+		
+		//close the context
+		context.close();
+	}
+
+}
