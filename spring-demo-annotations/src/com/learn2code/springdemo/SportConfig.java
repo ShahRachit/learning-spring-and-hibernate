5920522dbe4b4953f413271639860f6f83635b72
rachit.s <rachit.s@RACHIT-SH.ah.zymrinc.com>
2022-03-01 16:17:19 +0530

    Spring configuration with Java code (@Configuration)

---
+++ spring-demo-annotations/src/com/learn2code/springdemo/SportConfig.java
@@ -0,0 +1,12 @@
+package com.learn2code.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.learn2code.springdemo")
+public class SportConfig {
+
+	
+	
+}
