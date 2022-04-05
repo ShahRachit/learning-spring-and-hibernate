adf39531aed7caa6e03b62bf1468befb8635fe32
ShahRachit <rachitshah93@gmail.com>
2022-04-05 19:10:17 +0530

    List Customers vid 284

---
+++ web-customer-tracker/src/com/learn2code/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.learn2code.springdemo.dao;
+
+import java.util.List;
+
+import com.learn2code.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+}
